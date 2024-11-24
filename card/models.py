05377882a5afe71722469b9ea35f098de30c3848
Gyuhwan Park★ <unstabler@unstabler.pl>
2024-11-24 17:33:20 +0900

    db/card: 카드 관련 DB 모델 정의 추가

---
+++ card/models.py
@@ -0,0 +1,123 @@
+from django.db import models
+
+from user.models import User
+
+# Create your models here.
+
+class OfficialCardAssetAuthor(models.Model):
+    name = models.CharField(max_length=64, null=False, blank=False)
+    description = models.CharField(max_length=128, null=False, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OfficialCardAssetGroup(models.Model):
+    title = models.CharField(max_length=32, null=False, blank=False)
+    description = models.CharField(max_length=128, null=False, blank=False)
+
+    author = models.ForeignKey(OfficialCardAssetAuthor, on_delete=models.CASCADE, related_name='asset_groups')
+
+    price = models.IntegerField(default=0)
+    free = models.BooleanField(default=False)
+
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OfficialCardAsset(models.Model):
+    group = models.ForeignKey(OfficialCardAssetGroup, on_delete=models.CASCADE, related_name='assets')
+
+    index = models.IntegerField(null=False, blank=False, default=0)
+
+    title = models.CharField(max_length=32, null=True, blank=True)
+    description = models.CharField(max_length=128, null=True, blank=True)
+
+    type = models.CharField(max_length=32, null=False, blank=False)
+    object_key = models.CharField(max_length=2048, null=False, blank=False)
+    public_url = models.CharField(max_length=2048, null=False, blank=False)
+
+    mimetype = models.CharField(max_length=128, null=False, blank=False)
+    size = models.IntegerField(null=False, blank=False)
+
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    banned_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class OfficialCardAssetPurchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(OfficialCardAssetGroup, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    refunded_at = models.DateTimeField(null=True, blank=True)
+
+    refund_reason = models.CharField(max_length=128, null=True, blank=True)
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=32, null=False, blank=False)
+    content = models.JSONField(null=False, blank=False)
+
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    banned_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class UserCardAsset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='asset_references')
+
+    type = models.CharField(max_length=32, null=False, blank=False)
+    object_key = models.CharField(max_length=2048, null=False, blank=False)
+    public_url = models.CharField(max_length=2048, null=False, blank=False)
+
+    mimetype = models.CharField(max_length=128, null=False, blank=False)
+    size = models.IntegerField(null=False, blank=False)
+
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    banned_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CardFlag(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='flags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='flagged_cards')
+
+    reason = models.CharField(max_length=128, null=False, blank=False)
+    user_description = models.TextField(null=True, blank=True)
+
+    resolved_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CardDistribution(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='distributions')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_cards')
+
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    altitude = models.FloatField(null=True, blank=True)
+    accuracy = models.FloatField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class CardVote(models.Model):
+    class VoteType(models.IntegerChoices):
+        UPVOTE = 1
+        DOWNVOTE = 2
+
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='votes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='voted_cards')
+
+    vote_type = models.IntegerField(choices=VoteType.choices)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
