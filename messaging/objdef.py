1a244d30353c8838136bbc72875150a2294fa5bb
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-01-01 23:18:58 +0900

    messaging: objdef 추가

---
+++ messaging/objdef.py
@@ -0,0 +1,44 @@
+from typing import List, Optional, TypedDict, Literal, Dict
+
+from dataclasses import dataclass, asdict
+
+from dacite import from_dict
+
+# Object definitions for DirectMessage.content JSON field
+
+"""typescript
+export type DirectMessageTextContent = {
+    type: 'text'
+    text: string
+} 
+
+export type DirectMessageAttachmentContent = {
+    type: 'attachment'
+    attachment_id: string
+}
+
+export type DirectMessageContent = DirectMessageTextContent | DirectMessageAttachmentContent
+"""
+
+DirectMessageTextContentType = Literal["text"]
+DirectMessageAttachmentContentType = Literal["attachment"]
+
+@dataclass
+class DirectMessageTextContent:
+    type: DirectMessageTextContentType
+    text: str
+
+@dataclass
+class DirectMessageAttachmentContent:
+    type: DirectMessageAttachmentContentType
+    attachment_id: str
+
+DirectMessageContent = DirectMessageTextContent | DirectMessageAttachmentContent
+
+def load_direct_message_content(data: Dict) -> DirectMessageContent:
+    if data["type"] == "text":
+        return from_dict(data_class=DirectMessageTextContent, data=data)
+    elif data["type"] == "attachment":
+        return from_dict(data_class=DirectMessageAttachmentContent, data=data)
+    else:
+        raise ValueError(f"Unknown content type: {data['type']}")
