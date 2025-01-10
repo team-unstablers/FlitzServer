ba155e16204be25bf9044504361e7cd706615c81
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-01-11 05:58:25 +0900

    user_auth: UserSession에 대한 역방향 레퍼런스 생성

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 from flitz.models import BaseModel
+from flitz.apns import APNS
+
 from user.models import User
 
 # Create your models here.
 class UserSession(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sessions')
     description = models.CharField(max_length=64)
 
     initiated_from = models.CharField(max_length=128, null=False, blank=False)
@@ -14,3 +16,11 @@ class UserSession(BaseModel):
 
     expires_at = models.DateTimeField(null=True, blank=True)
     invalidated_at = models.DateTimeField(null=True, blank=True)
+
+    def send_push_message(self, title: str, body: str, data: dict):
+        """
+        사용자에게 푸시 메시지를 보냅니다.
+        """
+
+        apns = APNS.default()
+        apns.send_notification(title, body, [self.apns_token], data)
