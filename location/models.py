c83da5b65056ae6392d03b24b986b9027c68118c
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-03-22 17:49:40 +0900

    location: Mixin으로 분리

--- location/models.py
+++ location/models.py
@@ -8,10 +8,19 @@ from user.models import User
 from location.utils.timezone import get_timezone_from_coordinates
 from location.utils.distance import measure_distance
 
+class LocationDistanceMixin:
+    def distance_to(self, other) -> float:
+        """
+        두 위치 정보 사이의 거리를 계산합니다. 킬로미터 단위입니다.
+        """
+        loc1 = (self.latitude, self.longitude)
+        loc2 = (other.latitude, other.longitude)
+
+        return measure_distance(loc1, loc2)
 
 # Create your models here.
 
-class UserLocation(models.Model):
+class UserLocation(models.Model, LocationDistanceMixin):
     class Meta:
         get_latest_by = 'created_at'
 
@@ -35,21 +44,12 @@ class UserLocation(models.Model):
     def timezone_obj(self) -> pytz.timezone:
         return pytz.timezone(self.timezone)
 
-    def __sub__(self, other) -> float:
-        """
-        두 위치 정보 사이의 거리를 계산합니다. 킬로미터 단위입니다.
-        """
-        loc1 = (self.latitude, self.longitude)
-        loc2 = (other.latitude, other.longitude)
-
-        return measure_distance(loc1, loc2)
-
 
 class DiscoverySession(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='discovery_session')
     is_active = models.BooleanField(default=False)
 
-class DiscoveryHistory(BaseModel):
+class DiscoveryHistory(BaseModel, LocationDistanceMixin):
     session = models.ForeignKey(DiscoverySession, on_delete=models.CASCADE, related_name='discovered')
     discovered = models.ForeignKey(DiscoverySession, on_delete=models.CASCADE, related_name='discovered_by')
 
@@ -59,7 +59,5 @@ class DiscoveryHistory(BaseModel):
     accuracy = models.FloatField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
-
-
 
 
