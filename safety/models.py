0966790f1df121009471fab4a3c7760e11c08ef9
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-03-22 20:38:23 +0900

    safety: 트리거에 evaluate(), perform_block() 추가

--- safety/models.py
+++ safety/models.py
@@ -1,4 +1,7 @@
-from django.db import models
+from idlelib.pyparse import trans
+from typing import Optional
+
+from django.db import models, transaction
 
 from flitz.models import BaseModel
 from safety.utils.phonehash import hash_phone_number
@@ -37,10 +40,38 @@ class UserContactsTrigger(BaseModel):
     연락처에 따른 사용자 차단 트리거 정보를 저장합니다.
     """
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contact_triggers')
 
     # sha256sum(salt + phone_number)
     phone_number_hashed = models.CharField(max_length=64, null=False, blank=False)
+    related_object = models.ForeignKey(UserBlock, on_delete=models.SET_NULL, null=True, blank=True)
 
     def set_phone_number(self, phone_number: str):
         self.phone_number_hashed = hash_phone_number(phone_number)
+
+    def evaluate(self) -> Optional[User]:
+        """
+        트리거를 평가합니다.
+        """
+
+        user = User.objects.filter(phone_number_hashed=self.phone_number_hashed)
+        return user.first()
+
+    def perform_block(self):
+        """
+        트리거에 의해 사용자를 차단합니다.
+        """
+
+        with transaction.atomic():
+            user = self.evaluate()
+
+            if user is None:
+                return
+
+            block = UserBlock.objects.create(user=user, blocked_by=self.user, reason=UserBlock.Reason.BY_TRIGGER)
+            self.related_object = block
+            self.save()
+
+
+
+
